686ab248ac3770b9af4de383badbed098dbdc668
ligang <ligang.666@bytedance.com>
2020-08-23 20:24:33 +0800

    feat: 数字范围按位与 todo

---
+++ src/io/github/lgxkdream/test/unit15/BitwiseAndOfNumbersRange.java
@@ -0,0 +1,28 @@
+package io.github.lgxkdream.test.unit15;
+
+/**
+ * @author Jacky Lee
+ * @version 1.0.0
+ * @title 201. 数字范围按位与
+ * @description https://leetcode-cn.com/problems/bitwise-and-of-numbers-range/
+ * @since 2020-08-23 09:15
+ */
+public class BitwiseAndOfNumbersRange {
+
+    public static void main(String[] args) {
+        /**
+         * 输出: 4
+         */
+        System.out.println(rangeBitwiseAnd(5, 7));
+        /**
+         * 输出: 0
+         */
+        System.out.println(rangeBitwiseAnd(0, 1));
+    }
+
+    public static int rangeBitwiseAnd(int m, int n) {
+        int result = 0;
+        return result;
+    }
+
+}
