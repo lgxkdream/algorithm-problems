9d4e9264dd1efd7c89754c80b81614721563596f
ligang.666 <ligang.666@bytedance.com>
2020-09-08 18:33:17 +0800

    feat: 组合 todo

---
+++ src/io/github/lgxkdream/test/unit16/Combinations.java
@@ -0,0 +1,33 @@
+package io.github.lgxkdream.test.unit16;
+
+import java.util.List;
+
+/**
+ * @author Jacky Lee
+ * @version 1.0.0
+ * @title 77. 组合
+ * @description https://leetcode-cn.com/problems/combinations/
+ * @since 2020-09-08 10:01
+ */
+public class Combinations {
+
+    public static void main(String[] args) {
+        /**
+         * 输出:
+         * [
+         *   [2,4],
+         *   [3,4],
+         *   [2,3],
+         *   [1,2],
+         *   [1,3],
+         *   [1,4],
+         * ]
+         */
+        System.out.println(combine(4, 2));
+    }
+
+    public static List<List<Integer>> combine(int n, int k) {
+        return null;
+    }
+
+}
