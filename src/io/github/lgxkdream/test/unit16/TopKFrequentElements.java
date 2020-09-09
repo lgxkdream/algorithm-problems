7a289593eb788a87831f5ad19470d7b6a902d6af
ligang.666 <ligang.666@bytedance.com>
2020-09-09 23:06:26 +0800

    feat: 前 K 个高频元素

--- src/io/github/lgxkdream/test/unit16/TopKFrequentElements.java
+++ src/io/github/lgxkdream/test/unit16/TopKFrequentElements.java
@@ -1,6 +1,7 @@
 package io.github.lgxkdream.test.unit16;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
@@ -16,16 +17,30 @@ public class TopKFrequentElements {
         /**
          * 输出: [1,2]
          */
-        System.out.println(topKFrequent(new int[]{1, 1, 1, 2, 2, 3}, 2));
+        System.out.println(Arrays.toString(topKFrequent(new int[]{1, 1, 1, 2, 2, 3}, 2)));
         /**
          * 输出: [1]
          */
-        System.out.println(topKFrequent(new int[]{1}, 1));
+        System.out.println(Arrays.toString(topKFrequent(new int[]{1}, 1)));
     }
 
     public static int[] topKFrequent(int[] nums, int k) {
-        Arrays.stream(nums).boxed().collect(Collectors.toMap(n -> n, n -> 1, Integer::sum));
-        return null;
+        Map<Integer, Integer> countMap = Arrays.stream(nums).boxed().collect(Collectors.toMap(Function.identity(), n -> 1, Integer::sum));
+        Queue<Integer> queue = new PriorityQueue<>(Comparator.comparingInt(countMap::get));
+        countMap.forEach((num, count) -> {
+            if (queue.size() < k) {
+                queue.offer(num);
+            } else if (count > countMap.get(queue.peek())) {
+                queue.poll();
+                queue.offer(num);
+            }
+        });
+        int[] result = new int[queue.size()];
+        int i = 0;
+        for (Integer num : queue) {
+            result[i++] = num;
+        }
+        return result;
     }
 
 }
