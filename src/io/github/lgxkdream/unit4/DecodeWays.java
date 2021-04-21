90d15ca1f3f2121f1ee3846ee2b1e5411c3c91e3
Jacky Lee <lgxkdream@foxmail.com>
2021-04-22 00:05:39 +0800

    feat: 解码方法

--- src/io/github/lgxkdream/unit4/DecodeWays.java
+++ src/io/github/lgxkdream/unit4/DecodeWays.java
@@ -38,7 +38,18 @@ public class DecodeWays {
      * 动态规划
      */
     public static int numDecodings(String s) {
-
+        int len = s.length();
+        int[] dp = new int[len + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= len; i++) {
+            if (s.charAt(i - 1) != '0') {
+                dp[i] = dp[i - 1];
+            }
+            if (i >= 2 && (s.charAt(i - 2) == '1' || s.charAt(i - 2) == '2' && s.charAt(i - 1) < '7')) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        return dp[len];
     }
 
 }
