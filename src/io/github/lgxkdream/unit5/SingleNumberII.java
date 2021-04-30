66a78572447bc61fd38966d685dcc58b5c80d181
ligang <ligang.666@bytedance.com>
2021-04-30 11:56:19 +0800

    feat: init 只出现一次的数字 II

---
+++ src/io/github/lgxkdream/unit5/SingleNumberII.java
@@ -0,0 +1,27 @@
+package io.github.lgxkdream.unit5;
+
+/**
+ * @author Jacky Lee
+ * @version 1.0.0
+ * @title 137. 只出现一次的数字 II
+ * @description https://leetcode-cn.com/problems/single-number-ii/
+ * @since 2021-04-30 11:50
+ */
+public class SingleNumberII {
+
+    public static void main(String[] args) {
+        /**
+         * 输出：3
+         */
+        System.out.println(singleNumber(new int[]{2,2,3,2}));
+        /**
+         * 输出：99
+         */
+        System.out.println(singleNumber(new int[]{0,1,0,1,0,1,99}));
+    }
+
+    public static int singleNumber(int[] nums) {
+        return 0;
+    }
+
+}
