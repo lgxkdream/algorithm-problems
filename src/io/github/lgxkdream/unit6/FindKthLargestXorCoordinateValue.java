ec24eaa49ec1c33aa755979173b4da162f6b0c5a
Jacky Lee <lgxkdream@foxmail.com>
2021-05-20 23:01:42 +0800

    feat: 找出第 K 大的异或坐标值

--- src/io/github/lgxkdream/unit6/FindKthLargestXorCoordinateValue.java
+++ src/io/github/lgxkdream/unit6/FindKthLargestXorCoordinateValue.java
@@ -1,5 +1,7 @@
 package io.github.lgxkdream.unit6;
 
+import java.util.PriorityQueue;
+
 /**
  * @author Jacky Lee
  * @version 1.0.0
@@ -29,8 +31,23 @@ public class FindKthLargestXorCoordinateValue {
         System.out.println(kthLargestValue(new int[][]{{5, 2}, {1, 6}}, 4));
     }
 
+    /**
+     * 动态规划+优先队列
+     */
     public static int kthLargestValue(int[][] matrix, int k) {
-        return 0;
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        int m = matrix.length, n = matrix[0].length;
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = dp[i - 1][j - 1] ^ dp[i][j - 1] ^ dp[i - 1][j] ^ matrix[i - 1][j - 1];
+                queue.add(dp[i][j]);
+                if (queue.size() > k) {
+                    queue.poll();
+                }
+            }
+        }
+        return queue.peek();
     }
 
 }
