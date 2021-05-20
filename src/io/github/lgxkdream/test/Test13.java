dac4eb194f4255253f39118ea5f52cda4c5e4b66
Jacky Lee <lgxkdream@foxmail.com>
2021-05-20 23:22:44 +0800

    fix:

--- src/io/github/lgxkdream/test/Test13.java
+++ src/io/github/lgxkdream/test/Test13.java
@@ -20,12 +20,26 @@ public class Test13 {
          * 解析: "the", "is", "sunny" 和 "day" 是出现次数最多的四个单词，
          *     出现次数依次为 4, 3, 2 和 1 次。
          */
-        System.out.println(topKFrequent(new String[]{"the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"}, 4));
-        System.out.println(topKFrequent(new String[]{"i", "love", "leetcode", "i", "love", "coding"}, 2));
+        // System.out.println(topKFrequent(new String[]{"the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"}, 4));
+        // System.out.println(topKFrequent(new String[]{"i", "love", "leetcode", "i", "love", "coding"}, 2));
+        System.out.println(topKFrequent(new String[]{"glarko", "zlfiwwb", "nsfspyox", "pwqvwmlgri", "qggx",
+                "qrkgmliewc", "zskaqzwo", "zskaqzwo", "ijy", "htpvnmozay", "jqrlad", "ccjel", "qrkgmliewc", "qkjzgws",
+                "fqizrrnmif", "jqrlad", "nbuorw", "qrkgmliewc", "htpvnmozay", "nftk", "glarko", "hdemkfr", "axyak",
+                "hdemkfr", "nsfspyox", "nsfspyox", "qrkgmliewc", "nftk", "nftk", "ccjel", "qrkgmliewc", "ocgjsu", "ijy",
+                "glarko", "nbuorw", "nsfspyox", "qkjzgws", "qkjzgws", "fqizrrnmif", "pwqvwmlgri", "nftk", "qrkgmliewc",
+                "jqrlad", "nftk", "zskaqzwo", "glarko", "nsfspyox", "zlfiwwb", "hwlvqgkdbo", "htpvnmozay", "nsfspyox",
+                "zskaqzwo", "htpvnmozay", "zskaqzwo", "nbuorw", "qkjzgws", "zlfiwwb", "pwqvwmlgri", "zskaqzwo", "qengse",
+                "glarko", "qkjzgws", "pwqvwmlgri", "fqizrrnmif", "nbuorw", "nftk", "ijy", "hdemkfr", "nftk", "qkjzgws",
+                "jqrlad", "nftk", "ccjel", "qggx", "ijy", "qengse", "nftk", "htpvnmozay", "qengse", "eonrg", "qengse",
+                "fqizrrnmif", "hwlvqgkdbo", "qengse", "qengse", "qggx", "qkjzgws", "qggx", "pwqvwmlgri", "htpvnmozay",
+                "qrkgmliewc", "qengse", "fqizrrnmif", "qkjzgws", "qengse", "nftk", "htpvnmozay", "qggx", "zlfiwwb", "bwp",
+                "ocgjsu", "qrkgmliewc", "ccjel", "hdemkfr", "nsfspyox", "hdemkfr", "qggx", "zlfiwwb", "nsfspyox", "ijy",
+                "qkjzgws", "fqizrrnmif", "qkjzgws", "qrkgmliewc", "glarko", "hdemkfr", "pwqvwmlgri"}, 14));
     }
 
     public static List<String> topKFrequent(String[] words, int k) {
         Map<String, Integer> countMap = Arrays.stream(words).collect(Collectors.toMap(Function.identity(), n -> 1, Integer::sum));
+        System.out.println(countMap);
         PriorityQueue<String> queue = new PriorityQueue<>((a, b) -> {
             if (countMap.get(b) == countMap.get(a)) {
                 return b.compareTo(a);
@@ -39,7 +53,15 @@ public class Test13 {
             }
         });
         List<String> res = new ArrayList<>();
-        queue.forEach(word -> res.add(word));
+        while (!queue.isEmpty()) {
+            res.add(queue.poll());
+        }
+        // 错误写法1
+        // queue.forEach(word -> res.add(word));
+        // 错误写法2
+        // for (String word : queue) {
+        //     res.add(word);
+        // }
         Collections.reverse(res);
         return res;
     }
