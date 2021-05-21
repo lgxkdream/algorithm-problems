4b77df10e32f3361d6b182a3af9a61f35eff1487
Jacky Lee <lgxkdream@foxmail.com>
2021-05-21 23:15:45 +0800

    feat: 不相交的线

--- src/io/github/lgxkdream/unit6/UncrossedLines.java
+++ src/io/github/lgxkdream/unit6/UncrossedLines.java
@@ -26,8 +26,22 @@ public class UncrossedLines {
         System.out.println(maxUncrossedLines(new int[]{1, 3, 7, 1, 7, 5}, new int[]{1, 9, 2, 5, 1}));
     }
 
+    /**
+     * 动态规划
+     */
     public static int maxUncrossedLines(int[] nums1, int[] nums2) {
-        return 0;
+        int len1 = nums1.length, len2 = nums2.length;
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        for (int i = 1; i < len1 + 1; i++) {
+            for (int j = 1; j < len2 + 1; j++) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[len1][len2];
     }
 
 }
