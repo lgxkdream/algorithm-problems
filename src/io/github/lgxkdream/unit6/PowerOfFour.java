4427821552a034ac71477694ef41ebe12e37bc58
Jacky Lee <lgxkdream@foxmail.com>
2021-05-31 22:09:40 +0800

    feat: 4的幂

---
+++ src/io/github/lgxkdream/unit6/PowerOfFour.java
@@ -0,0 +1,31 @@
+package io.github.lgxkdream.unit6;
+
+/**
+ * @author Jacky Lee
+ * @version 1.0.0
+ * @title 342. 4的幂
+ * @description https://leetcode-cn.com/problems/power-of-four/
+ * @since 2021-05-31 22:07
+ */
+public class PowerOfFour {
+
+    public static void main(String[] args) {
+        /**
+         * 输出：true
+         */
+        System.out.println(isPowerOfFour(16));
+        /**
+         * 输出：false
+         */
+        System.out.println(isPowerOfFour(5));
+        /**
+         * 输出：true
+         */
+        System.out.println(isPowerOfFour(1));
+    }
+
+    public static boolean isPowerOfFour(int n) {
+        return false;
+    }
+
+}
