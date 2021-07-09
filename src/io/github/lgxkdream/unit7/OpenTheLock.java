36ab771c399d04a3bfa08c3ced0bc6ee6c1fe840
Jacky Lee <lgxkdream@foxmail.com>
2021-07-09 21:54:11 +0800

    feat: 打开转盘锁

--- src/io/github/lgxkdream/unit7/OpenTheLock.java
+++ src/io/github/lgxkdream/unit7/OpenTheLock.java
@@ -1,5 +1,7 @@
 package io.github.lgxkdream.unit7;
 
+import java.util.*;
+
 /**
  * @author Jacky Lee
  * @version 1.0.0
@@ -9,7 +11,7 @@ package io.github.lgxkdream.unit7;
  */
 public class OpenTheLock {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         /**
          * 输入：deadends = ["0201","0101","0102","1212","2002"], target = "0202"
          * 输出：6
@@ -40,8 +42,65 @@ public class OpenTheLock {
         System.out.println(openLock(new String[]{"0000"}, "8888"));
     }
 
-    public static int openLock(String[] deadends, String target) {
-        return 0;
+    public static int openLock(String[] deadends, String target) throws InterruptedException {
+        if ("0000".equals(target)) {
+            return 0;
+        }
+        List<String> deadendList = Arrays.asList(deadends);
+        if (deadendList.contains(target) || deadendList.contains("0000")) {
+            return -1;
+        }
+        Queue<String> queue = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
+        queue.offer("0000");
+        visited.add("0000");
+        int res = 0;
+        while (!queue.isEmpty()) {
+            res++;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String poll = queue.poll();
+                List<String> list = getList(poll);
+                for (String s : list) {
+                    if (!visited.contains(s) && !deadendList.contains(s)) {
+                        if (s.equals(target)) {
+                            return res;
+                        }
+                        queue.offer(s);
+                        visited.add(s);
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+
+    private static List<String> getList(String s) {
+        List<String> res = new ArrayList<>();
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < s.length(); i++) {
+            char c = chars[i];
+            chars[i] = getPre(c);
+            res.add(new String(chars));
+            chars[i] = getNext(c);
+            res.add(new String(chars));
+            chars[i] = c;
+        }
+        return res;
+    }
+
+    private static char getPre(char c) {
+        if (c == '0') {
+            return '9';
+        }
+        return (char) (c - 1);
+    }
+
+    private static char getNext(char c) {
+        if (c == '9') {
+            return '0';
+        }
+        return (char) (c + 1);
     }
 
 }
