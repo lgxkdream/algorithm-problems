174215647b49fec9c5457286b22390a379ec006b
Jacky Lee <lgxkdream@foxmail.com>
2021-09-30 21:03:10 +0800

    feat: 路径总和 III

--- src/io/github/lgxkdream/unit10/PathSumIII.java
+++ src/io/github/lgxkdream/unit10/PathSumIII.java
@@ -2,6 +2,9 @@ package io.github.lgxkdream.unit10;
 
 import io.github.lgxkdream.common.TreeNode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Jacky Lee
  * @version 1.0.0
@@ -21,11 +24,33 @@ public class PathSumIII {
         TreeNode n7 = new TreeNode(11);
         TreeNode n8 = new TreeNode(-3, null, n7);
         TreeNode n9 = new TreeNode(10, n6, n8);
+        /**
+         * 输出：3
+         */
         System.out.println(pathSum(n9, 8));
     }
 
+    /**
+     * 前缀和
+     */
     public static int pathSum(TreeNode root, int targetSum) {
-        return 0;
+        Map<Integer, Integer> prefix = new HashMap<>();
+        prefix.put(0, 1);
+        return getSum(root, targetSum, prefix, 0);
+    }
+
+    private static int getSum(TreeNode root, int targetSum, Map<Integer, Integer> prefix, int cur) {
+        if (root == null) {
+            return 0;
+        }
+        cur += root.val;
+        int res = 0;
+        res += prefix.getOrDefault(cur - targetSum, 0);
+        prefix.put(cur, prefix.getOrDefault(cur, 0) + 1);
+        res += getSum(root.left, targetSum, prefix, cur);
+        res += getSum(root.right, targetSum, prefix, cur);
+        prefix.put(cur, prefix.getOrDefault(cur, 0) - 1);
+        return res;
     }
 
 }
