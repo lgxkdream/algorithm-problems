75bc9b786f50a73c623a3d008017df97bc6c2b53
Jacky Lee <lgxkdream@foxmail.com>
2022-02-15 00:13:28 +0800

    feat: 有序数组中的单一元素

--- src/io/github/lgxkdream/unit11/SingleElementInASortedArray.java
+++ src/io/github/lgxkdream/unit11/SingleElementInASortedArray.java
@@ -23,19 +23,18 @@ public class SingleElementInASortedArray {
     public static int singleNonDuplicate(int[] nums) {
         int left = 0, right = nums.length - 1;
         while (left < right) {
-            int total = right - left + 1;
             int mid = left + (right - left) / 2;
-            if (nums[mid] == nums[mid + 1]) {
-                if ((total / 2) % 2 == 0) {
-                    left = mid;
+            if (mid % 2 == 0) {
+                if (nums[mid] == nums[mid + 1]) {
+                    left = mid + 1;
                 } else {
                     right = mid;
                 }
             } else {
-                if ((total / 2) % 2 == 0) {
-                    right = mid;
+                if (nums[mid] == nums[mid - 1]) {
+                    left = mid + 1;
                 } else {
-                    left = mid;
+                    right = mid;
                 }
             }
         }
